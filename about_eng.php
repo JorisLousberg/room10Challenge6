81c45d77b776b590f036af83f90982009f54d19e
Stan Erens <406294@vistacollege.nl>
2021-03-01 10:41:11 +0100

    .

---
+++ about_eng.php
@@ -0,0 +1,97 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>VistaCars</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+				<header id="header">
+					<div class="inner">
+
+						<!-- Logo -->
+							<a href="index.html" class="logo">
+								<span class="fa fa-car"></span> <span class="title">VistaCars</span>
+							</a>
+
+						<!-- Nav -->
+							<nav>
+								<ul>
+									<li><a href="#menu">Menu</a></li>
+								</ul>
+							</nav>
+
+					</div>
+				</header>
+
+			<!-- Menu -->
+				<nav id="menu">
+					<h2>Menu</h2>
+					<ul>
+						<li><a href="index.html">Home</a></li>
+
+						<li><a href="cars.html">Cars</a></li>
+
+						<li>
+							<a href="#" class="dropdown-toggle">Over Ons</a>
+
+							<ul>
+								<li class="active"><a href="about.html">Over Ons</a></li>
+								<li><a href="team.html">Team</a></li>
+								<li><a href="faq.html">FAQ</a></li>
+							</ul>
+						</li>
+						<li><a href="contact.html">Contact Ons</a></li>
+					</ul>
+				</nav>
+
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+							<h1>Over Ons</h1>
+
+							<div class="image main">
+								<img src="images/vistacars.JPG" class="img-fluid" alt="" />
+							</div>
+
+							<p>At VistaCars, we believe that our relationship does not end with buying you a new car. It's just the beginning of a long-term relationship that requires constant support and quality experience.</p>
+							<p>VistaCars is Maastricht's largest car dealer serving customers in the city from 3 locations and providing a comfortable and superior experience.</p>
+							<p>We invite you to discover the truly integrated car purchasing service that VistaCars has to offer through the largest car network in the Netherlands.</p>
+						</div>
+					</div>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<div class="inner">
+							<section>
+								<ul class="icons">
+									<li><a href="#" class="icon style2 fa-facebook"><span class="label">Facebook</span></a></li>
+									<li><a href="#" class="icon style2 fa-instagram"><span class="label">Instagram</span></a></li>
+								</ul>
+
+								&nbsp;
+							</section>
+
+							<ul class="copyright">
+								<li>Copyright © 2020 VistaCars </li>
+							</ul>
+						</div>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/main.js"></script>
+	</body>
+</html>
