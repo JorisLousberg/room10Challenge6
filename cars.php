d09d02fc213ef9f0c37200fecfed502a73b3b8a8
Stan Erens <406294@vistacollege.nl>
2021-03-04 09:13:58 +0100

    .

--- cars.php
+++ cars.php
@@ -94,7 +94,7 @@ require "navbar.php";
 
 							<ul class="copyright">
 								<li>Copyright © 2020 VistaCars </li>
-								<li><a href="team_eng.html">
+								<li><a href="cars_eng.php">
                             <img border="0" alt="Translation" src="images/brflag.webp" width="100" height="50"></li>
 							</ul>
 						</div>
--- cars_eng.php
+++ cars_eng.php
@@ -94,7 +94,7 @@ require "navbar.php";
 
 							<ul class="copyright">
 								<li>Copyright © 2020 VistaCars </li>
-								<li><a href="team.html">
+								<li><a href="cars.php">
                             <img border="0" alt="Translation" src="images/dflag.png" width="100" height="50"></li>
 							</ul>
 						</div>
